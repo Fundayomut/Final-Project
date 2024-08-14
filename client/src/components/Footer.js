28465d40e14567ddbad616384b4977061234e56a
Funda <fyomut@yahoo.com>
2024-08-14 16:07:50 +0200

    responsive design

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -23,7 +23,6 @@ function Footer() {
                 <img src='https://cdn2.iconfinder.com/data/icons/line-drawn-social-media/30/instagram-256.png' width="35px" height="35px" />
                 <img src='https://cdn4.iconfinder.com/data/icons/social-media-black-white-2/1227/X-64.png' width="30px" height="30px" />
                 <img src='https://cdn2.iconfinder.com/data/icons/line-drawn-social-media/31/yotube1-64.png' width="45px" height="45px" />
-
             </div>
             <div className='footerCoby'>
                 <p>All right recived 2024 </p>
--- client/src/components/NavNach.js
+++ client/src/components/NavNach.js
@@ -1,36 +1,65 @@
-import React from 'react'
-import bootstrap from 'bootstrap'
+import React from "react";
+import bootstrap from "bootstrap";
 
 export const NavNach = () => {
   return (
-        <div className='navVor'>
-            <div className='logo'>
-            <img src='/kuchen-boutiqe-high-resolution-logo-transparent.png' width="200px" height="100px" />
-            </div>
-            <div className='navVorBoot'>
-                <ul class="nav justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-Home" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-Order" href="#">Order</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-Contact" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-Profile" href="#">Profile</a>
-                    </li>
-                </ul>
-            </div>
-            <div className='search'>
-                <div className=''>
-                    <input className='inputsearch' placeholder='search' style={{height:"25px"}} />
-                    <img src='https://cdn0.iconfinder.com/data/icons/essentials-4/1687/search-512.png' width="20px" height="20px" style={{marginLeft:"10px"}} />
-                </div>
-                <img src='https://cdn4.iconfinder.com/data/icons/multimedia-75/512/multimedia-12-512.png' width="25px" height="25px" />
-                <img src='https://cdn4.iconfinder.com/data/icons/basic-user-interface-elements/700/exit-enter-leave-door-out-64.png' width="25px" height="25px" />
-            </div>
+    <div className="navVor">
+      <div className="logo">
+        <img
+          src="/kuchen-boutiqe-high-resolution-logo-transparent.png"
+          width="200px"
+          height="100px"
+        />
+      </div>
+      <div className="navVorBoot">
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-Home" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-Order" href="#">
+              Order
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-Contact" href="#">
+              Contact
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-Profile" href="#">
+              Profile
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="search">
+        <div className="navinputlink">
+          <input
+            className="inputsearch"
+            placeholder="search"
+            style={{ height: "25px" }}
+          />
+          <img
+            src="https://cdn0.iconfinder.com/data/icons/essentials-4/1687/search-512.png"
+            width="20px"
+            height="20px"
+            style={{ marginLeft: "10px"}}
+          />
         </div>
-  )
-}
+        <img
+          src="https://cdn4.iconfinder.com/data/icons/multimedia-75/512/multimedia-12-512.png"
+          width="25px"
+          height="25px"
+        />
+        <img
+          src="https://cdn4.iconfinder.com/data/icons/basic-user-interface-elements/700/exit-enter-leave-door-out-64.png"
+          width="25px"
+          height="25px"
+        />
+      </div>
+    </div>
+  );
+};
