4697937cd659fcb771e1a483dd8aadcaac85ac2b
Funda <fyomut@yahoo.com>
2024-08-20 18:18:05 +0200

    user profile Fehler behoben

--- client1/src/components/ProfileDetails.js
+++ client1/src/components/ProfileDetails.js
@@ -1,31 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { AuthKontext } from './LoginSystem';
 import { TextAntwort } from './ServerCom';
 import { useContext } from 'react';
 
 
-export const ProfileDetails = () => {
+export const ProfileDetails = ({
+  userNickName,
+  userFirstName,
+  userLastName,
+  eMail,
+  phone,
+  adresLine1,
+  adresLine2,
+  city,
+  state,
+  postalCode,
+  country,
+  password
+}) => {
      
-    const [userFirstName,setUserFirstName]=useState();
-    const [userLastName,setUserLastName]=useState();
-    const [userNickName,setUserNickname]=useState();
-    const [eMail,setEmail]=useState();
-    const [password,setPassword]=useState();
-    const [adresLine1,setAdresLine1]=useState();
-    const [adresLine2,setAdresLine2]=useState();
-    const [city,setCity]=useState();
-    const [state,setState]=useState();
-    const [postalCode,setPostalCode]=useState();
-    const [phone,setPhone]=useState();
-    const [country,setCountry]=useState();
-    const [userType,setUserType]=useState();
+    const [userUpFirstName,setUserUpFirstName]=useState(userFirstName);
+    const [userUpLastName,setUserUpLastName]=useState(userLastName);
+    const [userUpNickName,setUserUpNickname]=useState(userNickName);
+    const [eMailUp,setEmailUp]=useState(eMail);
+    const [passwordUp,setPasswordUp]=useState(password);
+    const [adresLine1Up,setAdresLine1Up]=useState(adresLine1);
+    const [adresLine2Up,setAdresLine2Up]=useState(adresLine2);
+    const [cityUp,setCityUp]=useState(city);
+    const [stateUp,setStateUp]=useState(state);
+    const [postalCodeUp,setPostalCodeUp]=useState(postalCode);
+    const [phoneUp,setPhoneUp]=useState(phone);
+    const [countryUp,setCountryUp]=useState(country);
+    const [userTypeUp,setUserTypeUp]=useState(0);
 
     const {userNumber}=useContext(AuthKontext);
 
+    useEffect(() => {
+        setUserUpNickname(userNickName);
+        setUserUpFirstName(userFirstName);
+        setUserUpLastName(userLastName);
+        setEmailUp(eMail);
+        setPasswordUp(password);
+        setAdresLine1Up(adresLine1 || '');
+        setAdresLine2Up(adresLine2 || '');
+        setCityUp(city || '');
+        setStateUp(state || '');
+        setPostalCodeUp(postalCode || '');
+        setPhoneUp(phone || '');
+        setCountryUp(country || '');
+    }, [userNickName, userFirstName, userLastName, eMail, phone, adresLine1, adresLine2, city, state, postalCode, country, password]);
+
 
     const updatePerson=()=>{
-        TextAntwort(`/user/update/${userNumber}/${userNickName}/${userFirstName}/${userLastName}/${userType}/${eMail}/${phone}/${adresLine1}/${adresLine2}/${city}/${state}/${postalCode}/${country}/${password}`,
+        TextAntwort(`/user/update/${userNumber}/${userUpNickName}/${userUpFirstName}/${userUpLastName}/${userTypeUp}/${eMailUp}/${phoneUp}/${adresLine1Up}/${adresLine2Up}/${cityUp}/${stateUp}/${postalCodeUp}/${countryUp}/${passwordUp}`,
             (res)=>{
                 console.log("Hinzugefuged",res)
             },
@@ -36,47 +64,22 @@ export const ProfileDetails = () => {
       }
           
   return (
-    <div>
-        <p>userNickName</p>
-        <input type='text' defaultValue={userNickName} onChange={(e)=>setUserNickname(e.target.value)}/>
-        <br/>
-        <p>userFirstName</p>
-        <input  type='text' defaultValue={userFirstName} onChange={(e)=>setUserFirstName(e.target.value)}/>
-        <br/>
-        <p>userLastName</p>
-        <input type='text' defaultValue={userLastName} onChange={(e)=>setUserLastName(e.target.value)}/>
-        <br/>
-        <p>userType</p>
-        <input type='text' defaultValue={userType} onChange={(e)=>setUserType(e.target.value)}/>
-        <br/>
-        <p >eMail</p>
-        <input  type='email' defaultValue={eMail} onChange={(e)=>setEmail(e.target.value)}/>
-        <br/>
-        <p>phone</p>
-        <input  type='phone' defaultValue={phone} onChange={(e)=>setPhone(e.target.value)}/>
-        <br/>
-        <p>adresLine1</p>
-        <input  type='text' defaultValue={adresLine1} onChange={(e)=>setAdresLine1(e.target.value)}/>
-        <br/>
-        <p>adresLine2</p>
-        <input  type='text' defaultValue={adresLine2} onChange={(e)=>setAdresLine2(e.target.value)}/>
-        <br/>
-        <p>city</p>
-        <input  type='text' defaultValue={city} onChange={(e)=>setCity(e.target.value)}/>
-        <br/>
-        <p>state</p>
-        <input  type='text' defaultValue={state} onChange={(e)=>setState(e.target.value)}/>
-        <br/>
-        <p>postalCode</p>
-        <input  type='text' defaultValue={postalCode} onChange={(e)=>setPostalCode(e.target.value)}/>
-        <br/>
-        <p>country</p>
-        <input  type='text' defaultValue={country} onChange={(e)=>setCountry(e.target.value)}/>
-        <br/>
-        <p>password</p>
-        <input  type='password' defaultValue={password} onChange={(e)=>setPassword(e.target.value)}/>
-        <br/>
-        <button onClick={updatePerson}>save change</button>
-    </div>
+    <>
+    <tr>
+       <td><input type='text' defaultValue={userNickName} onKeyUp={(e)=>setUserUpNickname(e.target.value)}/></td> 
+       <td><input  type='text' defaultValue={userFirstName} onKeyUp={(e)=>setUserUpFirstName(e.target.value)}/></td> 
+       <td><input type='text' defaultValue={userLastName} onKeyUp={(e)=>setUserUpLastName(e.target.value)}/></td> 
+       <td><input  type='email' defaultValue={eMail} onKeyUp={(e)=>setEmailUp(e.target.value)}/></td> 
+       <td><input  type='phone' defaultValue={phone} onKeyUp={(e)=>setPhoneUp(e.target.value)}/></td> 
+       <td><input  type='text' defaultValue={adresLine1} onKeyUp={(e)=>setAdresLine1Up(e.target.value)}/></td> 
+       <td><input  type='text' defaultValue={adresLine2} onKeyUp={(e)=>setAdresLine2Up(e.target.value)}/></td> 
+       <td><input  type='text' defaultValue={city} onKeyUp={(e)=>setCityUp(e.target.value)}/></td> 
+       <td><input  type='text' defaultValue={state} onKeyUp={(e)=>setStateUp(e.target.value)}/></td> 
+       <td><input  type='text' defaultValue={postalCode} onKeyUp={(e)=>setPostalCodeUp(e.target.value)}/></td> 
+       <td><input  type='text' defaultValue={country} onKeyUp={(e)=>setCountryUp(e.target.value)}/></td> 
+       <td><input  type='password' defaultValue={password} onKeyUp={(e)=>setPasswordUp(e.target.value)}/></td> 
+    </tr>
+    <button onClick={updatePerson}>save change</button>
+    </>
   )
 }
