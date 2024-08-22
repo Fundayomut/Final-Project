7371c569f9ec57a3673e5a4e23b4910e73e25c0f
Funda <fyomut@yahoo.com>
2024-08-22 14:23:04 +0200

    card details add und show

--- client1/src/components/Warenkorb.js
+++ client1/src/components/Warenkorb.js
@@ -1,38 +1,8 @@
 import React from 'react'
-import { ObjectAntwort } from './ServerCom';
-import { useState,useEffect } from 'react';
 
 const Warenkorb = () => {
-    const [orderList, setOrderList] = useState([]);
-
-    const orderListAbruf = () => {
-      ObjectAntwort(
-        `/orders/abruf/alle`,
-        (res) => {
-          setOrderList(res);
-          console.log(res);
-        },
-        (fehler) => {
-          console.log(fehler);
-        }
-      );
-    };
-
-useEffect(()=>{
-orderListAbruf()
-},[])
-
   return (
-    <div>
-        {orderList.map((item)=>(
-            <div key={item.orderNumber}>
-            <p><b>order Number</b> {item.orderNumber}</p>
-            <p><b>user Number</b>{item.userNumber}</p>
-            <p><b>order date</b>{item.orderDate}</p>
-            <p><b>total amount</b>{item.totalAmount}</p>
-            </div>
-        ))}
-    </div>
+    <div>Warenkorb</div>
   )
 }
 
